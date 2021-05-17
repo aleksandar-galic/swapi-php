f77ac10f28defbba5d5aa35ae03b5593eabe9dda
Aleksandar Galić <aleksandar.galic@quantox.com>
2021-05-17 14:34:42 +0200

    add find method

--- src/Swapi.php
+++ src/Swapi.php
@@ -13,4 +13,14 @@ class Swapi
 	{
 		return Http::get('https://swapi.dev/api/planets/')->json()['results'];
 	}
+
+	/**
+	 * @param $entity
+	 * @param $id
+	 * @return array|mixed
+	 */
+	public static function find($entity, $id)
+	{
+		return Http::get("https://swapi.dev/api/{$entity}/{$id}/")->json();
+	}
 }
